2902a7e69f7e66bb388ae679bb2aac0a69bbcea0
Otonda1 <charlesotonda@gmai.com>
2024-02-01 23:32:19 +0000

    redo

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
 """
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
+Module to check if provided dataset
+is in UTF-8 encoding
 """
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
 
 
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
-
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
-
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+    """Method to validate UTF-8 encoding"""
+    remaining_bytes = 0
+    for num in data:
+        if remaining_bytes == 0:
+            if num >> 7 == 0b0:
+                remaining_bytes = 0
+            elif num >> 5 == 0b110:
+                remaining_bytes = 1
+            elif num >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif num >> 3 == 0b11110:
+                remaining_bytes = 3
+            else:
                 return False
-            if byte[0:2] != [1, 0]:
+        else:
+            if num >> 6 != 0b10:
                 return False
-    return True
+            remaining_bytes -= 1
+        if remaining_bytes < 0:
+            return False
+
+        return remaining_bytes == 0
